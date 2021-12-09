5a3f6c357783ec2ca73c57369c35e43c060408d7
belikcost <k.belik@worksolutions.ru>
2021-12-09 16:33:15 +0300

    Общий функционал вынесен в утилиты

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,12 @@ import { defineComponent, Ref, ref, watch } from "vue";
 import { OperationsEnums } from "@/enums";
 import { SettingsInterface, TaskInterface } from "@/types";
 import StorageHelper from "@/domain/StorageHelper";
+import {
+  calculateAnswer,
+  generateRandomValue,
+  getSecondsFromMinutes,
+  sortOperationsByLogic,
+} from "./utils";
 
 interface AppStateInterface {
   tasks: TaskInterface[];
@@ -40,94 +46,24 @@ export default defineComponent({
     };
   },
   methods: {
-    onChangeCurrentTask(changedTask: TaskInterface) {
-      this.$data.tasks[this.tasks.length - 1] = changedTask;
-    },
-    createTask() {
-      const operations = this.settings.operations;
-      const level = this.settings.level;
-
-      const task: TaskInterface = {
-        firstNumber: this.generateRandomValue(Math.random, Math.floor),
-        operations: [],
-        answer: null,
-      };
-
-      while (task.operations.length < level) {
-        operations.forEach((operation) => {
-          task.operations.push({
-            type: operation,
-            correctValue: this.generateRandomValue(Math.random, Math.floor),
-            value: null,
-          });
-        });
-      }
-
-      task.operations = this.sortOperationsByLogic(task.operations);
-
-      task.answer = this.calculateAnswer(task.firstNumber, task.operations);
-
-      this.$data.tasks.push(task);
-    },
-    generateRandomValue(
-      randomizeFunction: () => number,
-      floorFunction: (arg0: number) => number
-    ) {
-      return floorFunction(randomizeFunction() * 100);
-    },
-    sortOperationsByLogic(operations: TaskInterface["operations"]) {
-      return [...operations].sort((a, b) => {
-        if ((a.type === 0 || a.type === 1) && (b.type === 0 || b.type === 1)) {
-          return 0;
-        } else {
-          return b.type - a.type;
-        }
-      });
-    },
-    getOperationResult(
-      firstOperand: number,
-      secondOperand: number,
-      operationType: OperationsEnums
-    ) {
-      switch (operationType) {
-        case OperationsEnums.addition:
-          return firstOperand + secondOperand;
-
-        case OperationsEnums.subtraction:
-          return firstOperand - secondOperand;
-
-        case OperationsEnums.multiplication:
-          return firstOperand * secondOperand;
-
-        case OperationsEnums.division:
-          return firstOperand / secondOperand;
-
-        case OperationsEnums.exponentiation:
-          return Math.pow(firstOperand, secondOperand);
-      }
-    },
-    calculateAnswer(
-      firstNumber: TaskInterface["firstNumber"],
-      operations: TaskInterface["operations"]
-    ) {
-      let result = firstNumber;
-
-      operations.forEach((operation) => {
-        const correctValue = operation.correctValue as number;
-
-        result = this.getOperationResult(result, correctValue, operation.type);
-      });
-
-      return result;
-    },
     startTheGame() {
-      this.$data.timer = this.getSecondsFromMinutes(this.settings.duration);
+      this.$data.timer = getSecondsFromMinutes(this.settings.duration);
 
       this.startTimer();
       this.createTask();
     },
-    getSecondsFromMinutes(minutes: number) {
-      return minutes * 60;
+    endTheGame() {
+      this.setPreviousTasks(this.tasks);
+      this.storageHelper.setToStorage(
+        "previousTasks",
+        JSON.stringify(this.tasks)
+      );
+
+      this.$data.tasks = [];
+      this.$data.timer = 0;
+      clearInterval(this.timerInterval);
+
+      this.$router.push("/start");
     },
     startTimer() {
       const updateTimer = () => {
@@ -142,18 +78,34 @@ export default defineComponent({
       const ONE_SECOND = 1000;
       this.$data.timerInterval = setInterval(updateTimer, ONE_SECOND);
     },
-    endTheGame() {
-      this.setPreviousTasks(this.tasks);
-      this.storageHelper.setToStorage(
-        "previousTasks",
-        JSON.stringify(this.tasks)
-      );
+    createTask() {
+      const operations = this.settings.operations;
+      const level = this.settings.level;
 
-      this.$data.tasks = [];
-      this.$data.timer = 0;
-      clearInterval(this.timerInterval);
+      const task: TaskInterface = {
+        firstNumber: generateRandomValue(Math.random, Math.floor),
+        operations: [],
+        answer: null,
+      };
 
-      this.$router.push("/start");
+      while (task.operations.length < level) {
+        operations.forEach((operation) => {
+          task.operations.push({
+            type: operation,
+            correctValue: generateRandomValue(Math.random, Math.floor),
+            value: null,
+          });
+        });
+      }
+
+      task.operations = sortOperationsByLogic(task.operations);
+
+      task.answer = calculateAnswer(task.firstNumber, task.operations);
+
+      this.$data.tasks.push(task);
+    },
+    onChangeCurrentTask(changedTask: TaskInterface) {
+      this.$data.tasks[this.tasks.length - 1] = changedTask;
     },
   },
   watch: {
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { OperationsEnums } from "@/enums";
+import { TaskInterface } from "@/types";
 
 const getOperationByType = (operationType: OperationsEnums): string => {
   switch (operationType) {
@@ -19,4 +20,72 @@ const getOperationByType = (operationType: OperationsEnums): string => {
   }
 };
 
-export { getOperationByType };
+const generateRandomValue = (
+  randomizeFunction: () => number,
+  floorFunction: (arg0: number) => number
+): number => {
+  return floorFunction(randomizeFunction() * 100);
+};
+
+const sortOperationsByLogic = (
+  operations: TaskInterface["operations"]
+): TaskInterface["operations"] => {
+  return [...operations].sort((a, b) => {
+    if ((a.type === 0 || a.type === 1) && (b.type === 0 || b.type === 1)) {
+      return 0;
+    } else {
+      return b.type - a.type;
+    }
+  });
+};
+
+const getOperationResult = (
+  firstOperand: number,
+  secondOperand: number,
+  operationType: OperationsEnums
+): number => {
+  switch (operationType) {
+    case OperationsEnums.addition:
+      return firstOperand + secondOperand;
+
+    case OperationsEnums.subtraction:
+      return firstOperand - secondOperand;
+
+    case OperationsEnums.multiplication:
+      return firstOperand * secondOperand;
+
+    case OperationsEnums.division:
+      return firstOperand / secondOperand;
+
+    case OperationsEnums.exponentiation:
+      return Math.pow(firstOperand, secondOperand);
+  }
+};
+
+const calculateAnswer = (
+  firstNumber: TaskInterface["firstNumber"],
+  operations: TaskInterface["operations"]
+): number => {
+  let result = firstNumber;
+
+  operations.forEach((operation) => {
+    const correctValue = operation.correctValue as number;
+
+    result = getOperationResult(result, correctValue, operation.type);
+  });
+
+  return result;
+};
+
+const getSecondsFromMinutes = (minutes: number): number => {
+  return minutes * 60;
+};
+
+export {
+  getOperationByType,
+  generateRandomValue,
+  sortOperationsByLogic,
+  getOperationResult,
+  calculateAnswer,
+  getSecondsFromMinutes,
+};
